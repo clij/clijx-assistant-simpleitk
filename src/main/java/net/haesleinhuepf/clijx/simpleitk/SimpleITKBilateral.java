d6ab660bf1132c99b79782a72fe6323b8a423a6d
haesleinhuepf <rhaase@mpi-cbg.de>
2020-10-01 14:56:31 +0200

    added Bilateral

---
+++ src/main/java/net/haesleinhuepf/clijx/simpleitk/SimpleITKBilateral.java
@@ -0,0 +1,72 @@
+package net.haesleinhuepf.clijx.simpleitk;
+
+
+import net.haesleinhuepf.clij.clearcl.ClearCLBuffer;
+import net.haesleinhuepf.clij.macro.CLIJMacroPlugin;
+import net.haesleinhuepf.clij.macro.CLIJOpenCLProcessor;
+import net.haesleinhuepf.clij.macro.documentation.OffersDocumentation;
+import net.haesleinhuepf.clij2.AbstractCLIJ2Plugin;
+import net.haesleinhuepf.clij2.CLIJ2;
+import net.haesleinhuepf.clij2.utilities.IsCategorized;
+import org.itk.simple.Image;
+import org.itk.simple.SimpleITK;
+import org.scijava.plugin.Plugin;
+
+import static net.haesleinhuepf.clijx.simpleitk.CLIJSimpleITKUtilities.*;
+
+@Plugin(type = CLIJMacroPlugin.class, name = "CLIJx_simpleITKBilateral")
+public class SimpleITKBilateral extends AbstractCLIJ2Plugin implements CLIJMacroPlugin, CLIJOpenCLProcessor, OffersDocumentation, IsCategorized
+{
+    @Override
+    public String getParameterHelpText() {
+        return "Image input, ByRef Image destination, Number domainSigma, Number rangeSigma, Number numberOfRangeGaussianSamples";
+    }
+
+    @Override
+    public Object[] getDefaultValues() {
+        return new Object[]{null, null, 4, 50, 100};
+    }
+
+    @Override
+    public boolean executeCL() {
+        boolean result = runAndCatch(() -> simpleITKBilateral(getCLIJ2(), (ClearCLBuffer) (args[0]), (ClearCLBuffer) (args[1]), asFloat(args[2]), asFloat(args[3]), asInteger(args[4])));
+        return result;
+    }
+
+    public static synchronized boolean simpleITKBilateral(CLIJ2 clij2, ClearCLBuffer input, ClearCLBuffer output, Float domainSigma, Float rangeSigma, Integer numberOfRangeGaussianSamples) {
+
+        // convert to ITK
+        Image itk_input = clijToITK(clij2, input);
+
+        // apply SimpleITK Median
+        Image itk_output = SimpleITK.bilateral(itk_input, domainSigma, rangeSigma, numberOfRangeGaussianSamples);
+
+        // push result back
+        ClearCLBuffer result = itkToCLIJ(clij2, itk_output);
+
+        // save it in the right place
+        clij2.copy(result, output);
+
+        // clean up
+        result.close();
+
+        return true;
+    }
+
+
+    @Override
+    public String getDescription() {
+        return "Apply SimpleITKs Bilateral filter to an image.\n\n" +
+                "See also: https://simpleitk.org/doxygen/latest/html/classitk_1_1simple_1_1BilateralImageFilter.html";
+    }
+
+    @Override
+    public String getAvailableForDimensions() {
+        return "2D, 3D";
+    }
+
+    @Override
+    public String getCategories() {
+        return "Filter";
+    }
+}
