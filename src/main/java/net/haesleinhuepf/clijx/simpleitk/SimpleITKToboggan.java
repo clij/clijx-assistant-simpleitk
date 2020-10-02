895f97cd1e9a2064c3abacfc1d7360b5c82e1ebe
haesleinhuepf <rhaase@mpi-cbg.de>
2020-10-02 16:16:25 +0200

    wip: Toboggan

---
+++ src/main/java/net/haesleinhuepf/clijx/simpleitk/SimpleITKToboggan.java
@@ -0,0 +1,68 @@
+package net.haesleinhuepf.clijx.simpleitk;
+
+
+import net.haesleinhuepf.clij.clearcl.ClearCLBuffer;
+import net.haesleinhuepf.clij.macro.CLIJMacroPlugin;
+import net.haesleinhuepf.clij.macro.CLIJOpenCLProcessor;
+import net.haesleinhuepf.clij.macro.documentation.OffersDocumentation;
+import net.haesleinhuepf.clij2.AbstractCLIJ2Plugin;
+import net.haesleinhuepf.clij2.CLIJ2;
+import net.haesleinhuepf.clij2.utilities.IsCategorized;
+import org.itk.simple.Image;
+import org.itk.simple.PixelIDValueEnum;
+import org.itk.simple.SimpleITK;
+import org.scijava.plugin.Plugin;
+
+import static net.haesleinhuepf.clijx.simpleitk.CLIJSimpleITKUtilities.*;
+
+@Plugin(type = CLIJMacroPlugin.class, name = "CLIJx_simpleITKToboggan")
+public class SimpleITKToboggan extends AbstractCLIJ2Plugin implements CLIJMacroPlugin, CLIJOpenCLProcessor, OffersDocumentation, IsCategorized
+{
+    @Override
+    public String getParameterHelpText() {
+        return "Image input, ByRef Image destination";
+    }
+
+    @Override
+    public boolean executeCL() {
+        boolean result = runAndCatch(() -> simpleITKToboggan(getCLIJ2(), (ClearCLBuffer) (args[0]), (ClearCLBuffer) (args[1])));
+        return result;
+    }
+
+    public static synchronized boolean simpleITKToboggan(CLIJ2 clij2, ClearCLBuffer input, ClearCLBuffer output) {
+
+        // convert to ITK
+        Image itk_input = clijToITK(clij2, input);
+        itk_input = SimpleITK.cast(itk_input, PixelIDValueEnum.sitkFloat32);
+
+        // apply Simple ITK Toboggan
+        Image itk_output = SimpleITK.toboggan(itk_input);
+
+        // push result back
+        ClearCLBuffer result = itkToCLIJ(clij2, itk_output);
+
+        // save it in the right place
+        clij2.binaryNot(result, output);
+
+        // clean up
+        result.close();
+
+        return true;
+    }
+
+
+    @Override
+    public String getDescription() {
+        return "Apply SimpleITKs Toboggan to an image.\n\n";
+    }
+
+    @Override
+    public String getAvailableForDimensions() {
+        return "2D, 3D";
+    }
+
+    @Override
+    public String getCategories() {
+        return "Binary,Segmentation";
+    }
+}
